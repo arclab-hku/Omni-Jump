7e78cc85a451e77ceac18b4a66fffa1bed479900
yinzhaodong <1447866357@qq.com>
2023-07-26 12:05:59 +0800

    add

--- scripts/eval_main.sh
+++
@@ -1,8 +0,0 @@
-maps=( resources/maps/Adrian0.png resources/maps/Adrian1.png resources/maps/Anaheim1.png resources/maps/Woonsocket0.png resources/maps/map1.png )
-for map in ${maps[@]}
-do
-  for ep_id in {0..9}
-  do
-    python legged_gym/scripts/play.py --task=aliengo_nav --episode-id $ep_id --map $map --seed $ep_id
-  done
-done
