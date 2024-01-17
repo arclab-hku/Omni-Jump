33f6005a953bc46450bc10148a84d9074db5a773
zerenluo <luozeren2@gmail.com>
2024-01-17 15:35:06 +0800

    add arcdog configs

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -7,11 +7,11 @@ from .base.legged_robot import LeggedRobot
 from .go1.go1 import Go1
 from .go1.go1_config_baseline import Go1BaseCfg, Go1BaseCfgPPO
 
-
-from .go1.go1 import Go1
-
 from .aliengo.aliengo import Aliengo
 from .aliengo.aliengo_config_baseline import AliengoBaseCfg, AliengoBaseCfgPPO
+
+from .arcdog.arcdog import Arcdog
+from .arcdog.arcdog_config_baseline import ArcdogBaseCfg, ArcdogBaseCfgPPO
 
 
 from legged_gym.utils.task_registry import task_registry
@@ -27,3 +27,4 @@ task_registry.register( "go1", Go1, Go1BaseCfg(), Go1BaseCfgPPO() )
 task_registry.register( "aliengo", Aliengo, AliengoBaseCfg(), AliengoBaseCfgPPO() )
 
 
+task_registry.register( "arcdog", Arcdog, ArcdogBaseCfg(), ArcdogBaseCfgPPO() )
---
+++ legged_gym/envs/arcdog/arcdog_config_baseline.py
@@ -0,0 +1,156 @@
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class ArcdogBaseCfg(LeggedRobotCfg):
+    class env(LeggedRobotCfg.env):
+        # num_envs = 4096
+        num_envs = 4096  # was getting a seg fault
+        # num_envs = 100  # was getting a seg fault
+        num_actions = 12
+        num_observations = 45
+        # num_proprio_obs = 48
+        camera_res = [1280, 720]
+        camera_type = "d"  # rgb
+        num_privileged_obs = 200  # 187
+        train_type = "EST"  # standard, priv, lbc, standard, RMA, EST, Dream, GenHis
+
+        follow_cam = False
+        float_cam = False
+
+        measure_obs_heights = False
+        num_env_priv_obs = 17  # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise
+        num_histroy_obs = 5
+
+
+    class terrain(LeggedRobotCfg.terrain):
+        mesh_type = 'trimesh'
+
+    class init_state(LeggedRobotCfg.init_state):
+        pos = [0.0, 0.0, 0.42]  # x,y,z [m]
+        default_joint_angles = {  # = target angles [rad] when action = 0.0
+            "FL_hip_joint": -0.05,  # [rad]
+            "RL_hip_joint": -0.05,  # [rad]
+            "FR_hip_joint": 0.05,  # [rad]
+            "RR_hip_joint": 0.05,  # [rad]
+
+            "FL_thigh_joint": 0.8,  # [rad]
+            "RL_thigh_joint": 1.0,  # [rad]
+            "FR_thigh_joint": 0.8,  # [rad]
+            "RR_thigh_joint": 1.0,  # [rad]
+
+            "FL_calf_joint": -1.5,  # [rad]
+            "RL_calf_joint": -1.5,  # [rad]
+            "FR_calf_joint": -1.5,  # [rad]
+            "RR_calf_joint": -1.5,  # [rad]
+        }
+
+    class control(LeggedRobotCfg.control):
+        # PD Drive parameters:
+        control_type = "P"
+        # stiffness = {'joint': 20.}  # [N*m/rad]
+        stiffness = {"joint": 45.0}  # [N*m/rad]
+        # damping = {'joint': 0.5}     # [N*m*s/rad]
+        damping = {"joint": 1.2}  # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+        use_actuator_network = True
+        actuator_net_file = "{LEGGED_GYM_ROOT_DIR}/resources/actuator_nets/unitree_aliengo_renet_aggre_it800.pt"
+
+    class asset(LeggedRobotCfg.asset):
+        file = "{LEGGED_GYM_ROOT_DIR}/resources/robots/arcdog/urdf/arcdog.urdf"
+        name = "arcdog"
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base", "trunk", "hip"]
+        self_collisions = 1  # 1 to disable, 0 to enable...bitwise filter
+
+    class domain_rand(LeggedRobotCfg.domain_rand):
+        randomize_base_mass = True
+        added_mass_range = [-3.0, 3.0]
+
+        randomize_friction = True
+        friction_range = [0.2, 1.25]
+
+        randomize_center = True
+        center_range = [-0.05, 0.05]
+
+        randomize_motor_strength = True
+        added_motor_strength = [0.9, 1.1]
+
+        randomize_lag_timesteps = True   # actuator net: True
+        added_lag_timesteps = 6
+
+        randomize_Motor_Offset = True  # actuator net: True
+        added_Motor_OffsetRange = [-0.02, 0.02]
+
+
+    class rewards(LeggedRobotCfg.rewards):
+        base_height_target = 0.5
+        max_contact_force = 500.0
+        only_positive_rewards = True
+        foot_height_target = 0.09
+
+        class scales(LeggedRobotCfg.rewards.scales):
+            tracking_lin_vel = 1.0
+            tracking_ang_vel = 0.5
+            lin_vel_z = -2.0
+            ang_vel_xy = -0.05
+            orientation = -0.2
+            torques = -0.00001
+            dof_acc = -2.5e-7
+            base_height = -0.0
+            feet_air_time = 1.0
+            collision = -1.0
+            action_rate = -0.01
+            # #### motion
+            f_hip_motion = -0.06
+            r_hip_motion = -0.06
+            f_thigh_motion = -0.04
+            r_thigh_motion = -0.04
+            f_calf_motion = -0.04
+            r_calf_motion = -0.04
+
+            #### smoothness
+            # dream_smoothness = -0.001
+            # power_joint = -1e-4
+            # foot_clearance = -0.001
+            # foot_height = -0.01
+
+
+    class evals(LeggedRobotCfg.evals):
+        feet_stumble = True
+        feet_step = True
+        crash_freq = True
+        any_contacts = True
+
+    class privInfo(LeggedRobotCfg.privInfo):
+        enableMotorStrength = True
+        enableMeasuredVel = True
+        enableMeasuredHeight = True
+        enableForce = True
+
+
+class ArcdogBaseCfgPPO(LeggedRobotCfgPPO):
+
+    class runner(LeggedRobotCfgPPO.runner):
+        run_name = ''
+        max_iterations = 4000  # number of policy updates
+        resume = False
+        save_interval = 50  # check for potential saves every this many iterations
+        experiment_name = 'arcdog'
+        export_policy = False
+        export_onnx_policy = False
+
+    class Encoder(LeggedRobotCfgPPO.Encoder):
+        priv_mlp_units = [258, 128, 3]
+        priv_info = False
+        priv_info_dim = 200
+        velLen = 3
+        proprio_adapt = False
+        checkpoint_model = None
+        proprio_adapt_out_dim = 11
+
+
+        HistoryLen = 5
+        Hist_info_dim = 45 * HistoryLen
